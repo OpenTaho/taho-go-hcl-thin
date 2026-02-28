aeb6801986cdfae1d8dfb737279119aa78d859d4
Mark Farnsworth <farnsworth2008@gmail.com>
2026-02-28 10:27:12 -0500

    wip

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/openTaho/taho-go-hcl-thin
 
 go 1.25.0
 
-require github.com/openTaho/taho-go-hcl v0.2.1 // indirect
+require github.com/openTaho/taho-go-hcl v0.2.1
 
 replace github.com/openTaho/taho-go-hcl => ../taho-go-hcl
--- parser.go
+++ parser.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	HclAlphaNumeric = iota
+	HclAlphaNumericDashOrUnderscore = iota
 	HclEquals
 	HclNewLine
 	HclOther
@@ -22,17 +22,31 @@ const (
 	HclUnknown
 )
 
-type HclThinElement struct {
-	hcl.HclElement
-
-	pair    hcl.HclElement
-	value   string
-	comment bool
-}
-
 type HclThinDir struct {
 	name   string
 	parser hcl.HclParser
+}
+
+type HclThinElement struct {
+	hcl.HclElement
+
+	isComment      bool
+	isString       bool
+	isHereDoc      bool
+	pair           hcl.HclElement
+	value          string
+	nestedElements []hcl.HclElement
+}
+
+func (h *HclThinElement) NestedElements() []hcl.HclElement {
+	if h.nestedElements == nil {
+		return []hcl.HclElement{}
+	}
+	return h.nestedElements
+}
+
+func (h *HclThinElement) SetNestedElements(elements []hcl.HclElement) {
+	h.nestedElements = elements
 }
 
 type HclThinFile struct {
@@ -40,49 +54,6 @@ type HclThinFile struct {
 }
 
 type HclThinParser struct{}
-
-func New() *HclThinParser {
-	return &HclThinParser{}
-}
-
-func (p HclThinParser) NewDir(name string) hcl.HclDir {
-	if !strings.HasPrefix(name, "/") {
-		wd, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-
-		name = filepath.Join(wd, name)
-	}
-
-	return &HclThinDir{
-		name:   name,
-		parser: p,
-	}
-}
-
-func (h *HclThinDir) Name() string {
-	return h.name
-}
-
-func (*HclThinDir) SetName(value string) {
-	panic("not implemented")
-}
-
-func (HclThinParser) NewFile(name string) hcl.HclFile {
-	if !strings.HasPrefix(name, "/") {
-		wd, err := os.Getwd()
-		if err != nil {
-			panic(err)
-		}
-
-		name = filepath.Join(wd, name)
-	}
-
-	return &HclThinFile{
-		name: name,
-	}
-}
 
 func (h *HclThinDir) Files() ([]hcl.HclFile, error) {
 	files, err := os.ReadDir(h.name)
@@ -108,6 +79,45 @@ func (h *HclThinDir) Files() ([]hcl.HclFile, error) {
 	return hclFiles, err
 }
 
+func (h *HclThinDir) Name() string {
+	return h.name
+}
+
+func (_ *HclThinDir) SetName(value string) {
+	panic("not implemented")
+}
+
+func (p HclThinParser) NewDir(name string) hcl.HclDir {
+	if !strings.HasPrefix(name, "/") {
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		name = filepath.Join(wd, name)
+	}
+
+	return &HclThinDir{
+		name:   name,
+		parser: p,
+	}
+}
+
+func (HclThinParser) NewFile(name string) hcl.HclFile {
+	if !strings.HasPrefix(name, "/") {
+		wd, err := os.Getwd()
+		if err != nil {
+			panic(err)
+		}
+
+		name = filepath.Join(wd, name)
+	}
+
+	return &HclThinFile{
+		name: name,
+	}
+}
+
 func (h *HclThinFile) Elements() []hcl.HclElement {
 	elements := []hcl.HclElement{}
 
@@ -117,33 +127,92 @@ func (h *HclThinFile) Elements() []hcl.HclElement {
 	}
 	defer f.Close()
 
+	fileElement := &HclThinElement{
+		value: "",
+	}
+	fileElement.SetNestedElements(readElements(f, err, fileElement.NestedElements()))
+	elements = append(elements, fileElement)
+
+	return elements
+}
+
+func readElements(f *os.File, err error, elements []hcl.HclElement) []hcl.HclElement {
 	c := HclUnknown
-	cc := HclAlphaNumeric
+	cc := HclAlphaNumericDashOrUnderscore
 	ignore := false
 	isComment := false
+	isString := false
 	pairing := false
 	pairingSoon := false
+	stringNext := false
 	pairingNext := false
 	pairingPending1 := false
 	pairingPending2 := false
+	hereDocExpected1 := false
+	hereDocExpected2 := false
 	br := bufio.NewReader(f)
+
+	var r rune
 	var sb strings.Builder
 
+	willBreak := false
 	for {
-		r, _, err := br.ReadRune()
-		willBreak := false
-		if err != nil {
-			r = ' '
-			willBreak = true
+		if stringNext {
+			stringNext = false
+			isString = true
+			readString(br, &sb)
+			ignore = true
+			r = 0
+		} else {
+			r, _, err = br.ReadRune()
+			if err != nil {
+				r = ' '
+				willBreak = true
+			}
 		}
 
-		if isAlphaNumeric(r) {
-			c = HclAlphaNumeric
+		if hereDocExpected2 {
+			hereDocExpected2 = false
+			ignore = true
+			sb.WriteRune(r)
+			readHereDoc(br, &sb)
+			if pairingNext {
+				pairingNext = false
+				newElement := &HclThinElement{
+					isHereDoc: true,
+					pair:      elements[len(elements)-5],
+					value:     sb.String(),
+				}
+				elements = pairWithLastElement(elements, newElement)
+			} else {
+				elements = append(elements, &HclThinElement{
+					value:     sb.String(),
+					isHereDoc: true,
+				})
+			}
+			sb.Reset()
+		} else if unicode.IsLetter(r) || unicode.IsNumber(r) || r == '-' || r == '_' {
+			c = HclAlphaNumericDashOrUnderscore
+		} else if r == '"' {
+			stringNext = true
+			c = HclOther
+			cc = HclUnknown
 		} else if r == '\n' {
 			c = HclNewLine
 			cc = HclUnknown
-		} else if isSpace(r) {
+		} else if unicode.IsSpace(r) {
 			c = HclSpace
+		} else if r == '<' {
+			c = HclOther
+			cc = HclUnknown
+			if hereDocExpected1 {
+				hereDocExpected1 = false
+				hereDocExpected2 = true
+			} else if pairingPending2 {
+				hereDocExpected1 = true
+				pairingPending2 = false
+				pairingPending1 = true
+			}
 		} else if r == '=' {
 			c = HclEquals
 			cc = HclUnknown
@@ -165,16 +234,22 @@ func (h *HclThinFile) Elements() []hcl.HclElement {
 
 		if cc != c {
 			v := sb.String()
+			if isString {
+				isString = false
+				v = strings.TrimPrefix(v, "\"")
+				v = strings.TrimSuffix(v, "\"")
+			}
 			if v != "" {
 				if pairing {
 					pairing = false
 					v = elements[len(elements)-1].Value()
-					elements = elements[:len(elements)-4]
-					last := len(elements) - 1
-					elements[last] = &HclThinElement{
-						value: v,
-						pair:  elements[last],
+					newElement := &HclThinElement{
+						isComment: isComment,
+						isString:  isString,
+						pair:      elements[len(elements)-5],
+						value:     v,
 					}
+					elements = pairWithLastElement(elements, newElement)
 				} else {
 					lastWasComment := len(elements) > 0 && elements[len(elements)-1].Comment()
 					if isComment && lastWasComment {
@@ -182,8 +257,8 @@ func (h *HclThinFile) Elements() []hcl.HclElement {
 						elements[len(elements)-1].SetValue(v)
 					} else {
 						elements = append(elements, &HclThinElement{
-							value:   v,
-							comment: isComment,
+							value:     v,
+							isComment: isComment,
 						})
 						if pairingNext {
 							pairingNext = false
@@ -222,6 +297,34 @@ func (h *HclThinFile) Elements() []hcl.HclElement {
 	return elements
 }
 
+func pairWithLastElement(elements []hcl.HclElement, newElement *HclThinElement) []hcl.HclElement {
+	elements = elements[:len(elements)-4]
+	last := len(elements) - 1
+	elements[last] = newElement
+	return elements
+}
+
+func readHereDoc(br *bufio.Reader, sb *strings.Builder) {
+	s, err := br.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	sb.WriteString(s)
+	end := strings.TrimPrefix(sb.String(), "<")
+	sb.Reset()
+	for true {
+		s, err = br.ReadString('\n')
+		if strings.HasSuffix(s, end) {
+			s = sb.String()
+			s = strings.TrimSuffix(s, "\n")
+			sb.Reset()
+			sb.WriteString(s)
+			break
+		}
+		sb.WriteString(s + "\n")
+	}
+}
+
 func readComment(br *bufio.Reader, sb *strings.Builder) {
 	s, err := br.ReadString('\n')
 	if err != nil {
@@ -230,12 +333,23 @@ func readComment(br *bufio.Reader, sb *strings.Builder) {
 	sb.WriteString(s)
 }
 
+func readString(br *bufio.Reader, sb *strings.Builder) {
+	s, err := br.ReadString('"')
+	if err != nil {
+		panic(err)
+	}
+	sb.WriteString(s)
+	if strings.HasSuffix(s, "\\\"") {
+		readString(br, sb)
+	}
+}
+
 func (h *HclThinElement) SetValue(value string) {
 	h.value = value
 }
 
 func (h *HclThinElement) Comment() bool {
-	return h.comment
+	return h.isComment
 }
 
 func (h *HclThinElement) Value() string {
@@ -258,10 +372,6 @@ func (*HclThinFile) SetName(value string) {
 	panic("not implemented")
 }
 
-func isAlphaNumeric(r rune) bool {
-	return unicode.IsLetter(r) || unicode.IsNumber(r)
-}
-
-func isSpace(r rune) bool {
-	return unicode.IsSpace(r)
+func New() *HclThinParser {
+	return &HclThinParser{}
 }
