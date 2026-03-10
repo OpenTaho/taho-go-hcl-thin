d7464b63032f88ead82a7dd21a67ec540295b0c8
Mark Farnsworth <farnsworth2008@gmail.com>
2026-03-10 06:46:11 -0400

    wip

--- parser.go
+++ parser.go
@@ -2,11 +2,13 @@ package parser
 
 import (
 	"bufio"
+	"cmp"
 	"log"
 	"math"
 	"os"
 	"path/filepath"
 	"regexp"
+	"slices"
 	"strings"
 	"unicode"
 
@@ -52,12 +54,38 @@ func (h *HclThinNode) SetType(value hcl.HclType) {
 	h.hclType = value
 }
 
+func (h *HclThinNode) IsSimplePair() bool {
+	if h.hclType != hcl.HclTypePair {
+		return false
+	}
+	if h.comment != "" {
+		return false
+	}
+	sb := &strings.Builder{}
+	for n := range h.nodes {
+		sb.WriteString(h.nodes[n].String())
+	}
+	v := sb.String()
+	if strings.Contains(v, "\n") {
+		return false
+	}
+	return true
+}
+
 func (h *HclThinNode) SetOperator(operator string) {
 	h.operator = operator
 }
 
 func (h *HclThinNode) Operator() string {
 	return h.operator
+}
+
+func (h *HclThinFile) String() string {
+	sb := &strings.Builder{}
+	for n := range h.body {
+		sb.WriteString(h.body[n].String())
+	}
+	return sb.String()
 }
 
 func (h *HclThinNode) Body() []hcl.HclNode {
@@ -74,6 +102,91 @@ func (h *HclThinNode) Type() hcl.HclType {
 
 type HclThinFile struct {
 	name string
+	body []hcl.HclNode
+}
+
+func (h *HclThinFile) SaveAs(fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+
+	file.WriteString(h.String())
+}
+
+func (h *HclThinFile) Format() hcl.HclFile {
+	if strings.HasSuffix(h.name, ".tfvars") {
+		return &HclThinFile{
+			name: h.name,
+			body: formatNodes(h.body[0].Body()),
+		}
+	}
+	return h
+}
+
+func formatNodes(nodes []hcl.HclNode) []hcl.HclNode {
+	pairs := []hcl.HclNode{}
+
+	for n := range nodes {
+		node := nodes[n]
+		if node.Type() == hcl.HclTypePair {
+			if nodes[n-1].Type() == hcl.HclTypeComment {
+				comment := nodes[n-1].Value()
+				node.SetComment(comment)
+			}
+			pairs = append(pairs, node)
+		}
+	}
+	fn := func(a, b hcl.HclNode) int {
+		return cmp.Compare(a.Value(), b.Value())
+	}
+	slices.SortFunc(pairs, fn)
+
+	simplePairs := []hcl.HclNode{}
+	complexPairs := []hcl.HclNode{}
+
+	for p := range pairs {
+		if pairs[p].IsSimplePair() {
+			simplePairs = append(simplePairs, pairs[p])
+		} else {
+			complexPairs = append(complexPairs, pairs[p])
+		}
+	}
+
+	pairGroup := &HclThinNode{
+		hclType: hcl.HclTypePairGroup,
+		nodes: []hcl.HclNode{
+			&HclThinNode{
+				hclType: hcl.HclTypePairGroup,
+				nodes:   simplePairs,
+			},
+			&HclThinNode{
+				hclType: hcl.HclTypePairGroup,
+				nodes:   complexPairs,
+			},
+		},
+	}
+
+	hasHeader := nodes[0].Type() == hcl.HclTypeComment &&
+		nodes[1].Type() == hcl.HclTypeSpace &&
+		nodes[1].Value() == "\n"
+
+	if hasHeader {
+		nodes = []hcl.HclNode{
+			&HclThinNode{
+				hclType: hcl.HclTypeSpace,
+				value:   "\n",
+				comment: nodes[0].Value(),
+			},
+			pairGroup,
+		}
+	} else {
+		nodes = []hcl.HclNode{
+			pairGroup,
+		}
+	}
+	return nodes
 }
 
 type HclThinParser struct{}
@@ -106,15 +219,11 @@ func (h *HclThinDir) Name() string {
 	return h.name
 }
 
-func (_ *HclThinDir) SetName(value string) {
-	panic("not implemented")
-}
-
 func (_ *HclThinDir) Body() []hcl.HclNode {
 	panic("not implemented")
 }
 
-func (p HclThinParser) NewDir(name string) hcl.HclDir {
+func (p *HclThinParser) NewDir(name string) hcl.HclDir {
 	if !strings.HasPrefix(name, "/") {
 		wd, err := os.Getwd()
 		if err != nil {
@@ -130,7 +239,7 @@ func (p HclThinParser) NewDir(name string) hcl.HclDir {
 	}
 }
 
-func (HclThinParser) NewFile(name string) hcl.HclFile {
+func (h *HclThinParser) NewFile(name string) hcl.HclFile {
 	if !strings.HasPrefix(name, "/") {
 		wd, err := os.Getwd()
 		if err != nil {
@@ -140,29 +249,33 @@ func (HclThinParser) NewFile(name string) hcl.HclFile {
 		name = filepath.Join(wd, name)
 	}
 
-	return &HclThinFile{
-		name: name,
-	}
-}
+	body := []hcl.HclNode{}
 
-func (h *HclThinFile) Body() []hcl.HclNode {
-	nodes := []hcl.HclNode{}
-
-	f, err := os.Open(h.name)
+	f, err := os.Open(name)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
 	fileElement := &HclThinNode{
-		value: filepath.Base(h.name),
+		value:   filepath.Base(name),
+		hclType: hcl.HclTypeBlock,
 	}
+
 	br := bufio.NewReader(f)
 	echo := &strings.Builder{}
 	fileElement.SetBody(readNodes(0, br, echo, fileElement.Body(), false))
-	nodes = append(nodes, fileElement)
 
-	return nodes
+	body = append(body, fileElement)
+
+	return &HclThinFile{
+		name: name,
+		body: body,
+	}
+}
+
+func (h *HclThinFile) Body() []hcl.HclNode {
+	return h.body
 }
 
 func readNodes(endRune rune, br *bufio.Reader, echo *strings.Builder, nodes []hcl.HclNode, exitOnEOL bool) []hcl.HclNode {
@@ -216,7 +329,9 @@ func readNodes(endRune rune, br *bufio.Reader, echo *strings.Builder, nodes []hc
 			}
 			nodes = append(nodes, n)
 		} else if r == endRune {
-			nodes = addNode(nodes, hclType, sb.String())
+			if sb.Len() > 0 {
+				nodes = addNode(nodes, hclType, sb.String())
+			}
 			willBreak = true
 		} else if isToken(r) {
 			c = HclAlphaNumericDashOrUnderscore
@@ -321,7 +436,7 @@ func readNodes(endRune rune, br *bufio.Reader, echo *strings.Builder, nodes []hc
 					n.SetType(hclType)
 					n.SetDocTag(docTag)
 					if hclType == hcl.HclTypeDocWithIndent {
-						lines := strings.Split(sb.String(), "\n")
+						lines := strings.Split(sb.String()+"\n", "\n")
 						lines = lines[:len(lines)-1]
 						lead := math.MaxInt32
 						for lineNum := range lines {
@@ -400,6 +515,7 @@ func readDoc(br *bufio.Reader, echo *strings.Builder, sb *strings.Builder) (hcl.
 	for true {
 		s, err = br.ReadString('\n')
 		if strings.HasSuffix(s, docTag+"\n") {
+			echo.WriteString(s)
 			s = sb.String()
 			s = strings.TrimSuffix(s, "\n")
 			sb.Reset()
@@ -407,9 +523,9 @@ func readDoc(br *bufio.Reader, echo *strings.Builder, sb *strings.Builder) (hcl.
 			break
 		}
 		sb.WriteString(s)
+		sb.WriteString("\n")
 		echo.WriteString(s)
 	}
-	sb.WriteString("\n")
 	return hclType, docTag
 }
 
@@ -489,24 +605,56 @@ func (h *HclThinNode) Value() string {
 }
 
 func (h *HclThinNode) String() string {
-	v := h.value
-	switch h.hclType {
-	case hcl.HclTypeString:
-		v = h.value
-	case hcl.HclTypeDoc:
-		v = "<<" + h.docTag + "\n" + h.value + h.docTag + "\n"
-	case hcl.HclTypeDocWithIndent:
-		v = "<<-" + h.docTag + "\n" + h.value + strings.Repeat(" ", h.docIndentation) + h.docTag + "\n"
+	sb := &strings.Builder{}
+	if h.hclType == hcl.HclTypePair && !h.IsSimplePair() {
+		sb.WriteRune('\n')
 	}
-	return v
+	if h.comment != "" {
+		sb.WriteString(h.comment)
+	}
+	switch h.hclType {
+	case hcl.HclTypeBlock:
+		fallthrough
+	case hcl.HclTypePairGroup:
+		for n := range h.nodes {
+			sb.WriteString(h.nodes[n].String())
+		}
+	case hcl.HclTypePair:
+		sb.WriteString(h.value)
+		sb.WriteString(" = ")
+		sbValue := &strings.Builder{}
+		for n := range h.nodes {
+			sbValue.WriteString(h.nodes[n].String())
+		}
+		sb.WriteString(strings.TrimSpace(sbValue.String()))
+		sb.WriteRune('\n')
+	case hcl.HclTypeDoc:
+		fallthrough
+	case hcl.HclTypeDocWithIndent:
+		sb.WriteString("<<-")
+		sb.WriteString(h.docTag)
+		sb.WriteString("\n")
+		sb.WriteString(h.value)
+		sb.WriteString(strings.Repeat(" ", h.docIndentation))
+		sb.WriteString(h.docTag)
+	case hcl.HclTypeString:
+		fallthrough
+	case hcl.HclTypeSpace:
+		fallthrough
+	case hcl.HclTypeToken:
+		sb.WriteString(h.value)
+	case hcl.HclTypeSpan:
+		sb.WriteString(h.value[0:1])
+		for n := range h.nodes {
+			sb.WriteString(h.nodes[n].String())
+		}
+		sb.WriteString(h.value[1:])
+	}
+	return sb.String()
 }
 
 func (h *HclThinFile) Name() string {
 	return h.name
-}
-
-func (*HclThinFile) SetName(value string) {
-	panic("not implemented")
 }
 
 func New() *HclThinParser {
